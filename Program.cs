7e059746f0b65cf1af9b4d0e2943ed4c647ffff5
Scott Slatton <scottslatton.tech@gmail.com>
2019-10-24 21:04:06 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Overriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adult scott = new Adult();
+            scott.Examine();
+        }
+    }
+
+    public class Patient
+    {
+        public virtual void Examine()
+        {
+            Console.WriteLine("The patient is being examined.");
+        }
+    }
+
+    public class Adult : Patient
+    {
+        public override void Examine()
+        {
+            Console.WriteLine("The Adult is being examined.");
+        }
+    }
+}
